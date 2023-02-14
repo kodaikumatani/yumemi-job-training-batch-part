473af328908c6d1ad1690df8129b7bda08b86543
kodaikumatani <koudai.aco5423@gmail.com>
2023-02-14 12:07:25 +0900

    refactor:Normalization made easier to see

--- app/Service/MailAnalysis.php
+++ app/Service/MailAnalysis.php
@@ -5,59 +5,64 @@ namespace App\Service;
 class MailAnalysis
 {
     /***
+     * Conversion of email text to first normalization.
+     *
      * @param $internal_date
      * @param $message
      * @return array|null[]
      */
     public static function regex($internal_date, $message): array
     {
-        $data = [];
-        $pattern = "/構成比\r\n-+\r\n|\n=+\r\n|\n\r\n【|】 【|】\r\n\r\n=+\r\n\r\n/";
+        $record = [];
+        $pattern = "/=+|構成比\r\n|\r\n【合計数量/";
         $split_message = preg_split($pattern,$message);
 
         if(str_contains($split_message[1], '売上データはありません。')) {
             return [NULL];
-        } else {
-            // Get proctor_id and proctor name
-            $split_header = preg_split("/\r\n/",$split_message[0]);
-            $producer_name = str_replace(" 様\r", '', $split_header[1]);
-            // Divide sales-info by store.
-            $stores = preg_split("/\n-+\r\n/", $split_message[2]);
-
-            foreach ($stores as $store) {
-                preg_match('/\(.+?\)/', $store, $match);
-                $date = self::editDate($internal_date, $match);
-                $store_name = preg_split('/\s:\s/',$store)[0];
-                $products = preg_split("/\n/",preg_split("/現在\)\r\n/",$store)[1]);
-
-                foreach ($products as $product)   {
-                    $items = preg_split("/\s+/", $product);
-                    $product_name = str_replace('（鳥取県産）','',$items[1]);
-                    $price = str_replace('円','',$items[2]);
-                    $quantity = str_replace('個','',$items[3]);
-
-                    $data[] = [
-                        'date' => $date,
-                        'hour' => date('H',strtotime($date)),
-                        'producer_name' => $producer_name,
-                        'store_name' => $store_name,
-                        'product_name' => $product_name,
-                        'price' => $price,
-                        'quantity' => $quantity,
-                    ];
-                }
-            }
-            return $data;
         }
+        // Get proctor_id and proctor name
+        $split_header = preg_split("/\r\n/",$split_message[0]);
+        $producer_name = str_replace(" 様", '', $split_header[1]);
+        // Divide sales-info by store.
+        $stores = preg_split("/-+\r\n/", $split_message[2]);
+
+        foreach (array_filter($stores) as $store) {
+            preg_match("{\((.*)\)}", $store, $match);
+            $date = self::formatDate($internal_date, $match[1]);
+            $store_name = preg_split('/\s:\s/',$store)[0];
+            $products = preg_split("/\n/",preg_split("/現在\)\r\n/",$store)[1]);
+
+            foreach (array_filter($products) as $product)   {
+                $items = preg_split("/\s+/", $product);
+                $product_name = str_replace('（鳥取県産）','',$items[1]);
+                $price = str_replace('円','',$items[2]);
+                $quantity = str_replace('個','',$items[3]);
+
+                $record[] = [
+                    'date' => $date,
+                    'producer_name' => $producer_name,
+                    'store_name' => $store_name,
+                    'product_name' => $product_name,
+                    'price' => $price,
+                    'quantity' => $quantity,
+                ];
+            }
+        }
+        return $record;
     }
 
-    private static function editDate($internal_date,$text): string
+    /**
+     * Formatting Japanese display to Y-m-d H:i.
+     *
+     * @param $internal_date
+     * @param $text
+     * @return string
+     */
+    private static function formatDate($internal_date,$text): string
     {
-        $text = str_replace('(',date('Y',$internal_date).'-',$text);
-        $text = str_replace('月', '-',$text);
-        $text = str_replace('日', ' ',$text);
-        $text = str_replace('時', ':',$text);
-        $text = str_replace('分現在)', ':00',$text);
-        return date('Y-m-d H:i',strtotime($text[0]));
+        $year = date('Y',$internal_date);
+        preg_match_all( '/\d+/',$text,$match);
+        $time = mktime($match[0][2], $match[0][3], 0, $match[0][0], $match[0][1], $year);
+        return date('Y-m-d H:i', $time);
     }
 }
