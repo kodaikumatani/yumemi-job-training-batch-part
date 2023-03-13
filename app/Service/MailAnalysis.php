88851e5d0ad224865a25bfa392624f66154f2895
kodaikumatani <koudai.aco5423@gmail.com>
2023-03-13 14:17:55 +0900

    fix:Bug in email normalization to go through

--- app/Service/MailAnalysis.php
+++ app/Service/MailAnalysis.php
@@ -2,6 +2,8 @@
 
 namespace App\Service;
 
+use Exception;
+
 class MailAnalysis
 {
     /***
@@ -13,44 +15,49 @@ class MailAnalysis
      */
     public static function regex($internal_date, $message): array
     {
-        $record = [];
-        $pattern = "/=+|構成比\r\n|\r\n【合計数量/";
-        $split_message = preg_split($pattern,$message);
+        try {
+            $record = [];
+            $pattern = "/=+|構成比\r\n|\r\n【合計数量/";
+            $split_message = preg_split($pattern,$message);
 
-        if(str_contains($split_message[1], '売上データはありません。')) {
+            if(str_contains($split_message[1], '売上データはありません。')) {
+                return [NULL];
+            }
+            // Get proctor_id and proctor name
+            $split_header = preg_split("/\r\n/",$split_message[0]);
+            $producer_code = str_replace('生産者コード:', '', $split_header[0]);
+            $producer_name = str_replace(' 様', '', $split_header[1]);
+            // Divide sales-info by store.
+            $stores = preg_split("/-+\r\n/", $split_message[2]);
+
+            foreach (array_filter($stores) as $store) {
+                preg_match("{\((.*)\)}", $store, $match);
+                $date = self::formatDate($internal_date, $match[1]);
+                $store_name = preg_split('/\s:\s/',$store)[0];
+                $products = preg_split("/\n/",preg_split("/現在\)\r\n/",$store)[1]);
+
+                foreach (array_filter($products) as $product)   {
+                    $items = preg_split("/\s+/", $product);
+                    $product_name = str_replace('（鳥取県産）','',$items[1]);
+                    $price = str_replace('円','',$items[2]);
+                    $quantity = str_replace('個','',$items[3]);
+
+                    $record[] = [
+                        'date' => $date,
+                        'producer_code' => $producer_code,
+                        'producer' => $producer_name,
+                        'store' => $store_name,
+                        'product' => $product_name,
+                        'price' => $price,
+                        'quantity' => $quantity,
+                    ];
+                }
+            }
+            return $record;
+        } catch (Exception $e)  {
+            echo $e->getMessage(), "\n";
             return [NULL];
         }
-        // Get proctor_id and proctor name
-        $split_header = preg_split("/\r\n/",$split_message[0]);
-        $producer_code = str_replace('生産者コード:', '', $split_header[0]);
-        $producer_name = str_replace(' 様', '', $split_header[1]);
-        // Divide sales-info by store.
-        $stores = preg_split("/-+\r\n/", $split_message[2]);
-
-        foreach (array_filter($stores) as $store) {
-            preg_match("{\((.*)\)}", $store, $match);
-            $date = self::formatDate($internal_date, $match[1]);
-            $store_name = preg_split('/\s:\s/',$store)[0];
-            $products = preg_split("/\n/",preg_split("/現在\)\r\n/",$store)[1]);
-
-            foreach (array_filter($products) as $product)   {
-                $items = preg_split("/\s+/", $product);
-                $product_name = str_replace('（鳥取県産）','',$items[1]);
-                $price = str_replace('円','',$items[2]);
-                $quantity = str_replace('個','',$items[3]);
-
-                $record[] = [
-                    'date' => $date,
-                    'producer_code' => $producer_code,
-                    'producer' => $producer_name,
-                    'store' => $store_name,
-                    'product' => $product_name,
-                    'price' => $price,
-                    'quantity' => $quantity,
-                ];
-            }
-        }
-        return $record;
     }
 
     /**
